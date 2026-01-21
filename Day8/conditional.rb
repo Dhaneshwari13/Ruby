cbe9e99f49d19b2dfbc3fdcba12218f6146014b1
Dhaneshwari <dhaneshwaribelle@gmail.com>
2026-01-21 15:32:56 +0530

    file upload

---
+++ Day8/conditional.rb
@@ -0,0 +1,114 @@
+#conditional statement
+
+# stock = 20
+# is_availabe = false
+# if stock > 0
+#     is_availabe = true
+# else
+#     puts "item not available ."
+# end
+
+# puts is_availabe
+
+
+#ternanry operator
+
+# is_active = false
+# puts is_active ? "item present" : "not present"
+
+
+#if – elsif – else
+
+# price = 300
+# discount = 0
+# if price > 100
+#     discount = 10
+# elsif price > 200
+#     discount = 20
+#     elsif price > 300
+#     discount = 30
+#     elsif price > 400
+#     discount = 40
+#     else
+#         discount = 5
+#     end
+#     puts discount
+
+#switch case
+# price = 250
+# discount=0
+# case 
+# when price >= 100 && price < 200
+#     discount = 10
+    
+# when price >= 200 && price < 300
+#     discount = 40
+  
+# when price >= 300 && price < 400
+#     discount = 30
+# when price >= 400 && price < 500
+#     discount = 40
+#     else
+#        discount = 10
+#     end
+#      puts discount
+
+
+# stock = 15
+# unless stock < 0 
+#     puts "stock available"
+#     else
+#         puts "stock"
+
+
+
+# email = true
+# unless email.nil?
+#     puts "email unavailable"
+# else
+#      puts "email available"
+# end
+
+email = nil 
+unless email.nil?
+    puts "email unavailable"
+else
+     puts "email available"
+end
+
+# class Animal
+#     def eat
+#         puts "animal is eating"
+#     end
+
+# protected
+# def run
+#     puts "animal is running"  #inside the class, to fetch outside the class it should be call from public methods  or sub class can access
+# end
+# end
+# class Dog < Animal
+#     def eat
+#         puts "dog is eating"
+#         sleep
+#         run
+#     end
+
+
+# private  #inside the class, to fetch outside the class it should be call from public methods 
+# def sleep
+#     puts "dog is sleeping"
+# end
+# end
+
+# dog1 = Dog.new
+# dog1.eat
+
+
+ 
+    
+    
+
+    
+
+
+
