21169687f943b0f74fa02ae92f1f3b1dd7cbb395
Dhaneshwari <dhaneshwaribelle@gmail.com>
2026-02-04 20:05:32 +0530

    updated file day18

--- complete_project/main_project/db/schema.rb
+++ complete_project/main_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_03_050034) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_04_042137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_03_050034) do
 
   create_table "product_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
